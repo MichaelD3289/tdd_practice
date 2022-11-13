575fc967394aa06d3e977471df688c6a7afc2d8d
Michael Drummond <michael.drummond328@gmail.com>
2022-11-13 10:32:31 -0700

    first 6 requirements of greeting function met

--- src/greeting/index.test.ts
+++ src/greeting/index.test.ts
@@ -21,11 +21,27 @@ describe('greeting', () => {
     expect(response).toBe('HELLO JERRY!')
   })
 
-  test('can handle multiple names', () => {
+  test('can handle two names', () => {
     const data = ['Jill', 'Jane']
 
     const response = greeting(data)
 
-    expect(response).toBe('Hello, Jill and Jane')
+    expect(response).toBe('Hello, Jill and Jane.')
+  })
+
+  test(' can handle arbitruary number of names, seperate with commas and close with oxford comma', () => {
+    const data = ['Amy', 'Brian', 'Charlotte']
+
+    const response = greeting(data)
+
+    expect(response).toBe('Hello, Amy, Brian, and Charlotte.')
+  })
+
+  test('allow mixing of normal and shouted names', () => {
+    const data = ['Amy', 'BRIAN', 'Charlotte']
+
+    const response = greeting(data)
+
+    expect(response).toBe('Hello, Amy and Charlotte. AND HELLO BRIAN!')
   })
 })
--- src/greeting/index.ts
+++ src/greeting/index.ts
@@ -9,7 +9,41 @@ function greeting(name: string[] | string = 'my friend'): string {
     return responseString
   }
 
-  return 'Hello, Jill and Jane'
+  if (Array.isArray(name)) {
+    let shoutedName = ''
+    const normalNames: string[] = []
+
+    name.forEach((n) => {
+      if (n.toUpperCase() === n) {
+        shoutedName = n
+        return
+      }
+      normalNames.push(n)
+    })
+
+    let nameConcat = ''
+    normalNames.forEach((n, index) => {
+      if (index === 0 && normalNames.length === 2) {
+        nameConcat += n + ' '
+        return
+      }
+
+      if (index === normalNames.length - 1) {
+        nameConcat += `and ${n}.`
+        return
+      }
+
+      nameConcat += `${n}, `
+    })
+
+    if (shoutedName !== '') {
+      nameConcat += ` AND HELLO ${shoutedName}!`
+    }
+
+    return `Hello, ${nameConcat}`
+  }
+
+  return ''
 }
 
 export default greeting
